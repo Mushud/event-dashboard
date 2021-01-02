79be04b8481d95fe25086d768fca6cd039517578
Mushud <mushudkofiabuu@gmail.com>
2021-01-02 15:30:05 +0000

    mm

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports ={
-    voucherServer: "https://ticket-icons.herokuapp.com",
+    voucherServer: "https://ticket-icons-2.herokuapp.com",
+    //"https://ticket-icons.herokuapp.com",
    // "http://192.168.100.94:3131",
     itmsApi: "https://api.mmt.itmsgh.com"
 }
