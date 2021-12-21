d04972af3b8220a9f1e16a954d5935530d4164be
Mushud <mushudkofiabuu@gmail.com>
2021-12-21 14:35:40 +0000

    login redesign

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,110 +1,322 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { toaster } from "evergreen-ui";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/appstore.png";
+import Button from "../components/Button";
+import { hyperApi } from "../config";
+import { useHistory } from "react-router-dom";
 
+export default function Login() {
+  const { push } = useHistory();
 
-export default function Login(){
-    return (
-        <div>
-              <div
-      style={{}}
-      class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8"
-    >
-      <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
-          class="mx-auto h-12 w-auto"
-          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-          alt="Workflow"
-        />
-        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          TicketIcon
-        </h2>
-        <h3 class="mt-6 text-center text-3xl font-bold text-gray-400">
-          Sign in to your account
-        </h3>
-      </div>
-      <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form class="space-y-6" action="#" method="POST">
+  const [loading, setLoading] = useState(false);
+
+  async function getEventByVerificationCode(verificationCode) {
+    setLoading(true);
+    try {
+      const nEvent = await axios.get(
+        hyperApi + "/events/one?verificationCode=" + verificationCode
+      );
+
+      toaster.success("Success");
+      sessionStorage.setItem("event", JSON.stringify(nEvent.data));
+      push("/dashboard");
+    } catch (e) {
+      toaster.danger("Invalid Event verification code");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <div class="pt-8 overflow-hidden sm:pt-12 lg:relative lg:py-48">
+        <div class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl lg:grid lg:grid-cols-2 lg:gap-24">
+          <div>
             <div>
-              <label
-                for="email"
-                class="block text-sm font-medium text-gray-700"
-              >
-                Email address
-              </label>
-              <div class="mt-1">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autocomplete="email"
-                  required
-                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
+              <img class="h-11 w-auto" src={Logo} alt="Workflow" />
+              <h1 class="text-2xl mt-5 font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                Hyper
+              </h1>
+              <h1 class="text-2xl mt-2 font-extrabold text-blue-500 tracking-tight sm:text-3xl">
+                Ticket Verification
+              </h1>
             </div>
-
-            <div>
-              <label
-                for="password"
-                class="block text-sm font-medium text-gray-700"
-              >
-                Password
-              </label>
-              <div class="mt-1">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autocomplete="current-password"
-                  required
-                  class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-
-            <div class="flex items-center justify-between">
-              <div class="flex items-center">
-                <input
-                  id="remember_me"
-                  name="remember_me"
-                  type="checkbox"
-                  class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                />
-                <label
-                  for="remember_me"
-                  class="ml-2 block text-sm text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-
-              <div class="text-sm">
-                <a
-                  href="#"
-                  class="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot your password?
+            <div class="mt-5">
+              <div>
+                <a href="#" class="inline-flex space-x-4">
+                  <span class="rounded bg-blue-50 px-2.5 py-1 text-xs font-semibold text-blue-500 tracking-wide uppercase">
+                    What's new
+                  </span>
+                  <span class="inline-flex items-center text-sm font-medium text-blue-500 space-x-1">
+                    <span>Just shipped version 0.1.0</span>
+                    {/* <!-- Heroicon name: solid/chevron-right --> */}
+                    <svg
+                      class="h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </span>
                 </a>
               </div>
-            </div>
+              <div class="mt-6 sm:max-w-xl">
+                <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  Scan and Verify your e-tickets with HyperQR
+                </h1>
+                <p class="mt-6 text-xl text-gray-500">
+                  Manage your event tickets with ease. Verify all sold tickets
+                  for both e-tickets and physical
+                </p>
+              </div>
+              <form
+                onSubmit={async (t) => {
+                  t.preventDefault();
 
-            <div>
-
-            
-            <Link to="/dashboard">
-              <button
-                type="submit"
-                class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  await getEventByVerificationCode(
+                    t.target.verificationCode.value
+                  );
+                }}
+                class="mt-12 sm:max-w-lg sm:w-full sm:flex"
               >
-                Sign in
-              </button>
-            </Link>
+                <div class="min-w-0 flex-1">
+                  <label for="hero-email" class="sr-only">
+                    Event Verification Code
+                  </label>
+                  <input
+                    id="hero-email"
+                    required
+                    name="verificationCode"
+                    type="text"
+                    class="block w-full border border-gray-300 rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                    placeholder="Enter Event Verification Code"
+                  />
+                </div>
+                <div class="mt-4 sm:mt-0 sm:ml-3">
+                  <Button loading={loading} title="Get Started" />
+                </div>
+              </form>
+              <div class="mt-6">
+                <div class="inline-flex items-center divide-x divide-gray-300">
+                  <div class="flex-shrink-0 flex pr-5">
+                    {/* <!-- Heroicon name: solid/star --> */}
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    {/* <!-- Heroicon name: solid/star --> */}
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    {/* <!-- Heroicon name: solid/star --> */}
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    {/* <!-- Heroicon name: solid/star --> */}
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                    {/* <!-- Heroicon name: solid/s,tar --> */}
+                    <svg
+                      class="h-5 w-5 text-yellow-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </div>
+                  <div class="min-w-0 flex-1 pl-5 py-1 text-sm text-gray-500 sm:py-3">
+                    <span class="font-medium text-gray-900">Rated 5 stars</span>{" "}
+                    by over{" "}
+                    <span class="font-medium text-blue-500">
+                      500 beta users
+                    </span>
+                  </div>
+                </div>
+              </div>
             </div>
-          </form>
+          </div>
+        </div>
+
+        <div class="sm:mx-auto sm:max-w-3xl sm:px-6">
+          <div class="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <div class="hidden sm:block">
+              <div class="absolute inset-y-0 left-1/2 w-screen bg-gray-50 rounded-l-3xl lg:left-80 lg:right-0 lg:w-full"></div>
+              <svg
+                class="absolute top-8 right-1/2 -mr-3 lg:m-0 lg:left-0"
+                width="404"
+                height="392"
+                fill="none"
+                viewBox="0 0 404 392"
+              >
+                <defs>
+                  <pattern
+                    id="837c3e70-6c3a-44e6-8854-cc48c737b659"
+                    x="0"
+                    y="0"
+                    width="20"
+                    height="20"
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x="0"
+                      y="0"
+                      width="4"
+                      height="4"
+                      class="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width="404"
+                  height="392"
+                  fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"
+                />
+              </svg>
+            </div>
+            <div class="relative pl-4 -mr-40 sm:mx-auto sm:max-w-3xl sm:px-0 lg:max-w-none lg:h-full lg:pl-12">
+              <img
+                class="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+                src="https://images.unsplash.com/photo-1556125574-d7f27ec36a06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* <div
+        style={{}}
+        class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8"
+      >
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+          <img
+            class="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+          <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            TicketIcon
+          </h2>
+          <h3 class="mt-6 text-center text-3xl font-bold text-gray-400">
+            Sign in to your account
+          </h3>
         </div>
-    )
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <form class="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  for="email"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div class="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    required
+                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  for="password"
+                  class="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+                <div class="mt-1">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autocomplete="current-password"
+                    required
+                    class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+
+              <div class="flex items-center justify-between">
+                <div class="flex items-center">
+                  <input
+                    id="remember_me"
+                    name="remember_me"
+                    type="checkbox"
+                    class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  />
+                  <label
+                    for="remember_me"
+                    class="ml-2 block text-sm text-gray-900"
+                  >
+                    Remember me
+                  </label>
+                </div>
+
+                <div class="text-sm">
+                  <a
+                    href="#"
+                    class="font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+              </div>
+
+              <div>
+                <Link to="/dashboard">
+                  <button
+                    type="submit"
+                    class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Sign in
+                  </button>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
 }
