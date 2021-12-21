3317ff12e8ff84ae60044e5706681b8f35a1af70
Mushud <mushudkofiabuu@gmail.com>
2021-12-21 14:35:01 +0000

    changed verification

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-module.exports ={
-    voucherServer: "https://ticket-icons.herokuapp.com",
-   // "http://192.168.100.94:3131",
-    itmsApi: "https://api.mmt.itmsgh.com"
-}
+module.exports = {
+  voucherServer: "https://ticket-icons.herokuapp.com",
+  // "http://192.168.100.94:3131",
+  itmsApi: "https://api.mmt.itmsgh.com",
+  hyperApi: "http://api.hyper.itmsgh.com",
+};
